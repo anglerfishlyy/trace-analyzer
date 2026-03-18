bbe520ac09dd56944b485dda9c12502ac79209d0
Kavya Samudrala <kavyalegitimate@gmail.com>
2026-03-18 14:47:15 +0530

    ock trace

--- main.go
+++ main.go
@@ -9,3 +9,30 @@ type Span struct {
 	Duration int // in ms
 	Children []*Span
 }
+
+func getMockTrace() *Span {
+	return &Span{
+		ID:       "1",
+		Name:     "gateway",
+		Duration: 100,
+		Children: []*Span{
+			{
+				ID:       "2",
+				Name:     "auth",
+				Duration: 120,
+				Children: []*Span{
+					{
+						ID:       "3",
+						Name:     "db",
+						Duration: 200,
+					},
+				},
+			},
+			{
+				ID:       "4",
+				Name:     "payment",
+				Duration: 300,
+			},
+		},
+	}
+}
